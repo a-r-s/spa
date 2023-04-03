21c670a67b88588e70175f38f8f0a4c50c8f1485
Agnieszka <agnrag@gmail.com>
2023-04-04 01:19:18 +0200

    add arrival and departure dates

--- src/views/RoomList.js
+++ src/views/RoomList.js
@@ -1,28 +1,32 @@
 // RoomList.js
 
-import { RoomDetails } from './RoomDetails';
-import { NavButton } from '../common/NavButton';
-import { cartManager } from '../cart/cart-manager';
+import { RoomDetails } from "./RoomDetails";
+import { NavButton } from "../common/NavButton";
+import { cartManager } from "../cart/cart-manager";
 
 export function RoomList() {
+  const section = document.createElement("section");
+  const ul = document.createElement("ul");
 
-  const section = document.createElement('section');
-  const ul = document.createElement('ul');
-  
   section.innerHTML = `
-    <h2>Room List</h2>
+    <h2>Lista dostępnych pokoi</h2>
+
+    <p>Określ daty pobytu:</p>
+    <input type="date" id="date-from" value="">
+    <input type="date" id="date-to" value="">
+
     <p>Sprawdź ofertę pokoi.</p>
     <p class="loading">Ładuję listę pokoi...</p>
   `;
 
   // pobieramy liste pokoi z serwera
-  fetch('http://localhost:3000/rooms')
-    .then(response => response.json())
-    .then(rooms => {
-        const lis = rooms.map(room => {
-          const li = document.createElement('li');
+  fetch("http://localhost:3000/rooms")
+    .then((response) => response.json())
+    .then((rooms) => {
+      const lis = rooms.map((room) => {
+        const li = document.createElement("li");
 
-          li.innerHTML = `
+        li.innerHTML = `
             <h4>${room.name}</h4>
             <p>
               <strong>${room.price.toFixed(2)} PLN</strong>
@@ -30,24 +34,61 @@ export function RoomList() {
             <footer></footer>
           `;
 
-          const addToCartButton = document.createElement('button');
-          addToCartButton.innerText = 'Dodaj do koszyka';
-          addToCartButton.classList.add('btn');
-          addToCartButton.addEventListener('click', () => cartManager.addItem(room));
+        const addToCartButton = document.createElement("button");
+        addToCartButton.innerText = "Dodaj do koszyka";
+        addToCartButton.classList.add("btn");
+        addToCartButton.addEventListener("click", () =>
+          cartManager.addItem(room)
+        );
 
-          const detailsButton = NavButton('Szczegóły...', () => RoomDetails(room.id), ['btn']);
-          
-          li.querySelector('footer').append(addToCartButton, detailsButton);
+        const detailsButton = NavButton(
+          "Szczegóły...",
+          () => RoomDetails(room.id),
+          ["btn"]
+        );
 
-          return li;
-        });
+        li.querySelector("footer").append(addToCartButton, detailsButton);
 
-        ul.append(...lis);
+        return li;
+      });
 
-        // usuwamy element mowiacy o ladowaniu
-        section.querySelector('.loading').remove();
-        // podstawiamy gotowa liste z pokojami
-        section.append(ul);
+      ul.append(...lis);
+
+      const inputDateFrom = document.getElementById("date-from");
+      const inputDateTo = document.getElementById("date-to");
+
+      inputDateFrom.setAttribute(
+        "value",
+        new Date().toISOString().slice(0, 10)
+      );
+      inputDateTo.setAttribute("value", new Date().toISOString().slice(0, 10));
+
+      inputDateFrom.setAttribute("min", new Date().toISOString().slice(0, 10));
+
+      inputDateFrom.addEventListener("change", () => {
+        inputDateTo.setAttribute("min", inputDateFrom.value);
+        inputDateTo.setAttribute("value", inputDateFrom.value);
+
+        const maxDate = new Date(inputDateFrom.value);
+        maxDate.setFullYear(maxDate.getFullYear() + 1);
+        inputDateTo.setAttribute("max", maxDate.toISOString().slice(0, 10));
+
+        if (
+          inputDateTo.value &&
+          inputDateTo.value > inputDateTo.getAttribute("max")
+        ) {
+          inputDateTo.value = "";
+        }
+      });
+
+      inputDateTo.addEventListener("change", () => {
+        if (inputDateTo.value < inputDateFrom.value) {
+          inputDateTo.value = inputDateFrom.value;
+        }
+      });
+
+      section.querySelector(".loading").remove();
+      section.append(ul);
     });
 
   return section;
