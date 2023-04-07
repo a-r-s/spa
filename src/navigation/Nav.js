d8b9c9f3e317caab1110d78bc919be5cfe0f7ce5
Agnieszka <agnrag@gmail.com>
2023-04-07 22:26:50 +0200

    add logo

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,23 +1,29 @@
-// Nav.js
-
-import { NavButton } from '../common/NavButton';
-import { Cart } from '../views/Cart';
-import { Home } from '../views/Home';
-import { RoomList } from '../views/RoomList';
-import { Treatments } from '../views/Treatments'
+import { NavButton } from "../common/NavButton";
+import { Cart } from "../views/Cart";
+import { Home } from "../views/Home";
+import { RoomList } from "../views/RoomList";
+import { Treatments } from "../views/Treatments";
 
 const navItems = [
-  { name: 'Home', component: Home },
-  { name: 'Rooms', component: RoomList },
-  { name: 'Zabiegi', component: Treatments },
-  { name: '🛒', component: Cart }
+  { name: "", component: Home, image: require("../assets/logo.png"), alt: 'logo' },
+  { name: "Rooms", component: RoomList },
+  { name: "Zabiegi", component: Treatments },
+  { name: "🛒", component: Cart },
 ];
 
 export function Nav() {
-  const nav = document.createElement('nav');
+  const nav = document.createElement("nav");
 
-  const navButtons = navItems.map(navItem => {
-    return NavButton(navItem.name, navItem.component, ['btn']);
+  const navButtons = navItems.map((navItem) => {
+    const button = NavButton(navItem.name, navItem.component, ["btn"]);
+    if (navItem.image) {
+      const navImg = document.createElement("img");
+      navImg.src = navItem.image;
+      navImg.style.height = "50px";
+      navImg.alt = navItem.alt;
+      button.prepend(navImg);
+    }
+    return button;
   });
 
   nav.append(...navButtons);
